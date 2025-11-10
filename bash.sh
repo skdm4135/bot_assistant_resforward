3fec895ed7b085e40eff95e37c03f359dc23bd37
skdm4135 <143496276+skdm4135@users.noreply.github.com>
2025-11-11 03:29:51 +0530

    updated bash.sh for port

--- bash.sh
+++ bash.sh
@@ -1,2 +1,13 @@
-echo "starting Bot ~@save_restricted";
-python3 -m main
+# echo "starting Bot ~@save_restricted";
+# python3 -m main
+
+=======================
+# new changes
+
+#!/bin/bash
+
+# Start dummy web server so Render doesn't timeout
+python3 server.py &
+
+# Start your Telegram bot
+python3 main/__main__.py
