e5479d94eebaca85beee951a7837c8f7c0f3c06d
skdm4135 <143496276+skdm4135@users.noreply.github.com>
2025-11-15 09:06:26 +0530

    changed name

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -1,4 +1,4 @@
-#Github.com/mrinvisible7
+j#Github.com/mrinvisible7
 
 import asyncio, time, os
 import logging
@@ -271,7 +271,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                     progress=progress_for_pyrogram,
                     progress_args=(
                         client,
-                        "**UPLOADING:**\n**bot made by Mr. Invisible**",
+                        "**UPLOADING:**\n**bot made by Mr. Sumit**",
                         upm,
                         time.time(),
                     ),
